3e3e4743551613e542c0003c94689dd5b0889b90
Archit Jain <arcyjain2002@gmail.com>
2024-06-16 23:03:38 +0530

    controlling fps etc

--- SimpleScene/script.js
+++ SimpleScene/script.js
@@ -12,8 +12,8 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.x = 2;
-camera.position.y = 1;
+camera.position.x = 0;
+camera.position.y = 0;
 scene.add(camera);
 
 const canvas = document.querySelector(".webgl");
@@ -22,4 +22,24 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+
+// the time part of the loop is to have a constant speed of the animation irrespect of the framerate difference between different systems
+let time = Date.now();
+
+//Animation
+let loop = () => {
+
+  // Time
+  let currentTime = Date.now();
+  let deltaTime = currentTime - time;
+  time = currentTime;
+
+  mesh.rotation.y = mesh.rotation.y + 0.001*deltaTime;
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(loop);
+}
+
+loop();
